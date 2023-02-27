09e38c776c18937c9e7f163057de4dff9a404915
George Brown <teawares@gmail.com>
2023-02-28 00:53:34 +0800

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -61,23 +61,7 @@ class Layout extends React.Component {
             阿江
           </a>
           <div className="social-links">
-            <a
-              href="https://twitter.com/anikijiang"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              twitter
-            </a>{" "}
-            &bull;{" "}
-            <a
-              href="https://instagram.com/veryben"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              instagram
-            </a>{" "}
-            &bull;{" "}
-            <a
+           <a
               href="https://github.com/teaware"
               target="_blank"
               rel="noopener noreferrer"
